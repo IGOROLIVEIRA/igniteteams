0955183d6277cd1f982b74d531976cd767a15ba8
Igor Afonso Oliveira Ruas <ruas.igor@gmail.com>
2023-06-10 10:41:49 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/theme';
+
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups /> : <ActivityIndicator/> }
+    </ThemeProvider>
   );
 }
 
