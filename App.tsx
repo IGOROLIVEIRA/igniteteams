92b440bb25658171e473a8a8941615a8ce3a05a9
Igor Ruas <ruas.igor@gmail.com>
2023-06-13 13:40:03 -0300

    criando padão de cores

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './src/theme';
 
+import { Loading } from '@components/Loading';
 import { Groups } from '@screens/Groups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator/> }
+      { fontsLoaded ? <Groups /> : <Loading/> }
     </ThemeProvider>
   );
 }
